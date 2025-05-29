b80096dc2737c57085569b78a5ed9a3a2c239711
Aijavai <17807080178@163.com>
2025-05-29 21:31:05 +0800

    coze 打造知识库智能体

--- batjtmd/js/5.js
+++ batjtmd/js/5.js
@@ -1,11 +1,13 @@
 // 全局作用域
 function fn(){ // 函数作用域
+    //console.log(a); // undefined 变量提升
+    console.log(b);
     let a = 2;
     if(true){ // let 支持块级作用域（高级语言的特性） var 不支持
         var b = 2;
-        let b = 3;
+       // let b = 3;
     }
-    console.log(b);
+   
 }
 fn();
 if(false) { // 块级作用域
