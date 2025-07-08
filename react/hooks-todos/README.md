5928f5f8b5c6c14a11fe5c13651d820ab3c0e8bc
Aijavai <17807080178@163.com>
2025-07-08 21:46:23 +0800

    react 数据绑定

--- react/hooks-todos/README.md
+++ react/hooks-todos/README.md
@@ -5,6 +5,11 @@
        css 超集
     - 拥有vite 脚手架
         stylus 预编译 安装stylus vite 直接编译
+、、、
+    npm install stylus stylus-loader --save-dev
+ 或
+    yarn add stylus stylus-loader -D
+、、、
         vite 来自vue 社区
     - react 组件设计
       - 开发任务单元
@@ -26,3 +31,17 @@
     - 移动端的重要单位 移动端不要用px px是像素点，是绝对单位
         移动端 是宽高不定的 使用相对单位 rem（root em） vw/vh(viewport)
         使用相对单位，可以在所有设备上适配
+
+- props   组件通信
+    - 传递状态
+    - 传递自定义事件
+    - 直接解构
+        const {
+            todos,  // 任务
+            onAddTodo  // 添加
+        } = props 单独解构
+
+- vue 和 react 区别
+    - vue 好入门，api好用
+    - react 倾向于原生JS  入门难
+        - hooks
--- react/hooks-todos/src/components/Todos/TodoForm.jsx
+++ react/hooks-todos/src/components/Todos/TodoForm.jsx
@@ -2,13 +2,32 @@ import {
     useState,   // 私有状态
 } from 'react';
 
-const TodoForm = () => {
-    // 一定要有唯一的最外层元素 树状结构，编译解析JSX,
+const TodoForm = ({onAddTodo}) => {
+    // 数据
+    // props 参数数据
+    // state 私有的数据
+    // 单项数据流
+    const [text, setText] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let result = text.trim();  // dry dont repeat yourself
+        if (!result) return;
+        onAddTodo(result);
+        setText('');  // 数据状态和界面状态一致要敏感
+    }
+    // JSX 一定要有唯一的最外层元素 树状结构，编译解析JSX,
     return (
    <>
     <h1 className="header">TodoList</h1>
-    <form >
-
+    <form className='todo-input' onSubmit={handleSubmit}>  {/** 表单提交事件,类型DOM0级事件 */}
+        <input 
+        type="text" 
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder='请输入待办事项'
+        required
+        />
+        <button type='submit'>Add</button>
     </form>
    </>
     )
